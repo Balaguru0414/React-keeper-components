a2a3f50cdb56b93f413cd41b2b17d57e56c34805
Balaguru0414 <balag0414@gmail.com>
2023-03-09 21:19:48 +0530

    Keeper app using component

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Note = function () {
+  return (
+    <div className="note">
+      <h1>I am Balaguru</h1>
+      <p>I am a Developer</p>
+    </div>
+  );
+};
+
+export default Note;
